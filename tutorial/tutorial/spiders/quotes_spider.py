1e50ed01ec49e21aadebcfee628ec3ed9d815e66
Elyse Kriegel <ekriegel@icetechnologies.com>
2021-03-31 17:57:11 -0500

    first commit

---
+++ tutorial/tutorial/spiders/quotes_spider.py
@@ -0,0 +1,23 @@
+import scrapy 
+
+
+class QuotesSpider(scrapy.Spider):
+    name = "quotes"
+
+    def start_requests(self):
+        urls = [
+            "http://quotes.toscrape.com/page/1",
+            "http://quotes.toscrape.com/page/2",
+        ]
+
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+        
+    def parse(self, reponse):
+        page = reponse.url.split('/')[-2]
+        filename = f'quotes-{page}.html'
+
+        with open(filename, 'wb') as f:
+            f.write(reponse.body)
+        
+        self.log(f'Saved file {filename}')
